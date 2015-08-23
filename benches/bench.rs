a6d0c21729ad5cc054108e816d098bb9dedf6030
Jonathan Nilsson <lord.jonil@gmail.com>
2015-08-23 20:04:18 +0200

    More tests

--- benches/bench.rs
+++ benches/bench.rs
@@ -4,10 +4,29 @@ extern crate persistent_hashmap;
 extern crate test;
 
 use persistent_hashmap::PersistentHashmap;
+use std::mem::size_of;
+use test::Bencher;
 
 #[bench]
 fn bench(b: &mut Bencher) {
+    
+    let capacity = 10*1024;
+    let size = 1*1024;
+
+    {
+        let mut db = PersistentHashmap::<u64, u64>::new("bench.db", capacity).unwrap();
+
+        for i in 0..size {
+            assert_eq!(db.insert(i, i), Ok(None));    
+        }
+    }
+
+    let db = PersistentHashmap::<u64, u64>::open("bench.db").unwrap();
+
+    b.bytes = size * size_of::<u64>() as u64;
     b.iter(|| {
-        PersistentHashmap::<u64>::new("bench.db", 1024).unwrap();
+        for i in 0..size {
+            assert_eq!(db.get(i), Some(i));    
+        }
     });
 }
