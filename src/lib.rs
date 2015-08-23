b6b72f43f0d9870fd022673400f4d51b8f50040b
Jonathan Nilsson <lord.jonil@gmail.com>
2015-08-23 13:36:46 +0200

    Actually inserting stuff into the map

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,8 @@
 
 extern crate persistent_array;
 
+use std::borrow::Borrow;
+use std::default::Default;
 use std::hash::{Hash, Hasher, SipHasher};
 use std::marker::{PhantomData, Reflect};
 use std::path::Path;
@@ -14,29 +16,40 @@ impl<T: Hash> KeyTypeBounds for T {}
 pub trait ValueTypeBounds: Copy + Default + Reflect + 'static {}
 impl<T: Copy + Default + Reflect + 'static> ValueTypeBounds for T {}
 
-#[derive(Copy, Clone, Default)]
-pub struct HashmapEntry<V> {
+#[derive(Clone, Copy)]
+enum EntryState {
+    Empty,
+    Occupied,
+}
+
+impl Default for EntryState {
+    fn default() -> EntryState { EntryState::Empty }
+}
+
+#[derive(Clone, Copy, Default)]
+struct HashmapEntry<V> {
     hash: u64,
-    is_occupied: bool,
+    state: EntryState,
     value: V,
 }
 
-pub struct PersistentHashmap<K: ?Sized + KeyTypeBounds, V: ValueTypeBounds> {
+pub struct PersistentHashmap<K: KeyTypeBounds, V: ValueTypeBounds> {
     phantom_type: PhantomData<K>,
     array: PersistentArray<HashmapEntry<V>>,
 }
 
-fn hash<T: Hash>(v: &T) -> u64 {
+fn hash<K: Hash>(v: K) -> u64 {
     let mut s = SipHasher::new();
     v.hash(&mut s);
     s.finish()
 }
 
-impl<K: ?Sized + KeyTypeBounds, V: ValueTypeBounds> PersistentHashmap<K, V> {
+impl<K: KeyTypeBounds, V: ValueTypeBounds> PersistentHashmap<K, V> {
 
     /// Creates a new persistent hashmap
     pub fn new<P>(path: P, size: u64) -> Result<PersistentHashmap<K, V>, Error>
             where P: AsRef<Path> {
+        
         let array = match PersistentArray::new(&path, size) {
             Ok(array) => array,
             Err(err) => return Err(err),
@@ -61,5 +74,48 @@ impl<K: ?Sized + KeyTypeBounds, V: ValueTypeBounds> PersistentHashmap<K, V> {
             phantom_type: PhantomData,
             array: array,
         })
+    }
+
+    pub fn insert(&mut self, k: K, v: V) -> Option<V> {
+        let (slot, hash) = self.get_slot_and_hash(k);
+
+        let entry = &mut self.array[slot as usize];
+
+        entry.hash = hash;
+        entry.state = EntryState::Occupied;
+
+        match entry.state {
+            EntryState::Empty => {
+                entry.value = v;
+                None
+            },
+            EntryState::Occupied => {
+                let old = entry.value;
+                entry.value = v;
+                Some(old)
+            },
+        }
+    }
+
+    pub fn get(&self, k: K) -> Option<V> {
+
+        let (slot, hash) = self.get_slot_and_hash(k);
+
+        let entry = &self.array[slot as usize];
+
+
+        match entry.state {
+            EntryState::Empty => {
+                None
+            },
+            EntryState::Occupied => {
+                Some(entry.value)
+            },
+        }
+    }
+
+    fn get_slot_and_hash(&self, k: K) -> (u64, u64) {
+        let hash = 0x8000_0000_0000_0000 | hash(k);
+        (hash % self.array.len() as u64, hash)
     }
 }
--- tests/test.rs
+++ tests/test.rs
@@ -5,17 +5,17 @@ use persistent_hashmap::PersistentHashmap;
 #[test]
 fn test() {
     {
-        let db = PersistentHashmap::<str, u64>::new("test.db", 1024).unwrap();
+        let mut db = PersistentHashmap::<&str, u64>::new("test.db", 1024).unwrap();
 
-        //db["a"] = 1;
-        //db["b"] = 2;
-        //db["c"] = 3;
+        db.insert("a", 1);
+        db.insert("b", 2);
+        db.insert("c", 3);
     }
     {
-        let db = PersistentHashmap::<str, u64>::open("test.db").unwrap();
+        let db = PersistentHashmap::<&str, u64>::open("test.db").unwrap();
 
-        //assert_eq!(db["a"], 1);
-        //assert_eq!(db["b"], 1);
-        //assert_eq!(db["c"], 1);
+        assert_eq!(db.get("a"), Some(1));
+        assert_eq!(db.get("b"), Some(2));
+        assert_eq!(db.get("c"), Some(3));
     }
 }
